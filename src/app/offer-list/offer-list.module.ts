ada1e79d53086ae4d2d5ba99c170389dc3c32327
Marcel Dybowski <mdybowski20@gmail.com>
2023-03-21 19:37:31 +0100

    adding search and offer list components

---
+++ src/app/offer-list/offer-list.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { OfferListRoutingModule } from './offer-list-routing.module';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    OfferListRoutingModule
+  ]
+})
+export class OfferListModule { }
