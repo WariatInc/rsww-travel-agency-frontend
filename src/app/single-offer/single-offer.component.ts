9f369fdc1050a74cdb0bd23560d0590ee933cf0e
Marcel Dybowski <mdybowski20@gmail.com>
2023-05-03 13:11:40 +0200

    simple single offer view

--- src/app/single-offer/single-offer.component.ts
+++ src/app/single-offer/single-offer.component.ts
@@ -9,8 +9,9 @@ import { Offer } from '../common/model/offer';
   styleUrls: ['./single-offer.component.css'],
 })
 export class SingleOfferComponent implements OnInit {
-  offerId: string | null | undefined;
-  offer: Offer | undefined;
+  private offerId: string | null | undefined;
+  public offer!: Offer;
+  public loadingOfferInfo: boolean = true;
 
   constructor(
     private offerService: OfferService,
@@ -20,6 +21,7 @@ export class SingleOfferComponent implements OnInit {
     this.offerId = this.route.snapshot.paramMap.get('id');
     this.offerService.getOfferInfo(this.offerId).subscribe((offer) => {
       this.offer = offer;
+      this.loadingOfferInfo = false;
     });
   }
 }
--- src/app/single-offer/single-offer.module.ts
+++ src/app/single-offer/single-offer.module.ts
@@ -3,9 +3,16 @@ import { CommonModule } from '@angular/common';
 
 import { SingleOfferRoutingModule } from './single-offer-routing.module';
 import { SingleOfferComponent } from './single-offer.component';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
+import { MatButtonModule } from '@angular/material/button';
 
 @NgModule({
   declarations: [SingleOfferComponent],
-  imports: [CommonModule, SingleOfferRoutingModule],
+  imports: [
+    CommonModule,
+    SingleOfferRoutingModule,
+    MatProgressSpinnerModule,
+    MatButtonModule,
+  ],
 })
 export class SingleOfferModule {}
