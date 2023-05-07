32146706c97e167eab7d75ec8ff647a149a24756
Mateusz Mazur <mateusz.mazur@e.email>
2023-05-07 19:52:07 +0200

    Add reservation tests

--- test/test_login.js
+++ test/test_login.js
@@ -2,17 +2,12 @@ const { Builder, By, Key, until } = require("selenium-webdriver");
 const assert = require("assert");
 const { describe, it } = require("mocha");
 
-const {
-  selenium_run,
-  get_login_button,
-  find_button_with_text,
-} = require("./util");
+const { selenium_run, get_login_button, login_with_email } = require("./util");
 
 const URL = "http://localhost:4200/";
 const EMAIL = "jeremias.sofia@gmail.com";
 
 describe("Login page", function () {
-  let login_page = null;
   it("Should be possible to go to login page", async function () {
     await selenium_run(async (driver) => {
       await driver.get(URL);
@@ -22,21 +17,13 @@ describe("Login page", function () {
       await driver.wait(until.urlContains("login"), 500);
       const next_url = await driver.getCurrentUrl();
       assert(current_url != next_url, "Url should be changed after click");
-      login_page = next_url;
     });
   });
 
   it("Should be possible to login", async function () {
     await selenium_run(async (driver) => {
-      await driver.get(login_page);
-      const email_field = await driver.wait(
-        until.elementLocated(By.xpath("//input[@type='search']")),
-        500
-      );
-
-      await email_field.sendKeys(EMAIL, Key.ENTER);
-      const submit_button = await find_button_with_text(driver, "Login");
-      await submit_button.click();
+      await driver.get(URL);
+      await login_with_email(driver, EMAIL);
 
       const email_div = await driver.wait(
         until.elementLocated(By.xpath(`//mat-toolbar/div[text()='${EMAIL}']`)),
